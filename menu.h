2b933de2c145c555e4a1c020bad77e5860a5eca7
ChanChakSum3035708534 <73269541+ChanChakSum3035708534@users.noreply.github.com>
2020-11-21 11:58:22 +0800

    added comments in the code

--- menu.h
+++ menu.h
@@ -10,17 +10,24 @@ WINDOW* til;
 
 int menu()
 {
+    // the character array that contains all the choices
     char choice_list[CHOICE][15]={"Play", "Instruction", "Leaderboard", "Exit"};
+    // the tempotaty array to store the choice to be printed out at a row
     char choice[15];
+    // inp is for input of user
     int inp, i = 0;
     
-    initscr();
+    // initializd the screen
+    initscr(); 
+    // the window for menu
     men = newwin(16, 25, 4, 1);
     box(men, 0, 0);
+    // the window for title
     til = newwin(3, 25, 1, 1);
     box(til, 0, 0);
     mvwprintw(til, 1, 1, "NS-SHAFT:C++");
     wrefresh(til);
+    // initially, print out the menu with the first box being highlighted
     for(i=0; i<CHOICE; i++)
     {
         if(i==0)
@@ -31,9 +38,11 @@ int menu()
         {
             wattroff(men, A_STANDOUT);
         }
+	// this puts the item in the choice list into the variable for storing the list of that row
         sprintf(choice, "%s", choice_list[i]);
         mvwprintw(men, i+1, 1, "%s", choice);
     }
+    // instructions of the menu usage
     mvwprintw(men, 10, 2, "Up  -> '%c' ", up);
     mvwprintw(men, 11, 2, "Down -> '%c' ", down);
     mvwprintw(men, 12, 2, "Select -> '%c' ", control_choose);
@@ -41,8 +50,10 @@ int menu()
     noecho();
     keypad(men, true);
     curs_set(0);
+    // obtain user input from the menu screen
     while(inp = wgetch(men))
     {
+	// this reprints the previous choice after the highlight is turned off
         sprintf(choice, "%s", choice_list[i]);
         mvwprintw(men, i+1, 1, "%s", choice);
         switch(inp)
@@ -61,19 +72,22 @@ int menu()
                 i = 0;
             }
             break;
+	    // the case when player chooses the current choice
             case control_choose:
+	    // the choice of play game/ returns 1 to the run() in gameManager
             if(i == 0)
             {
                 return 1;
             }
+            // for the instruction choice the instruction window pops out
             if(i == 1)
             {
                 ins = newwin(14 , 55, 4, 27);
                 box(ins, 0, 0);
                 mvwprintw(ins, 2, 2, "Controls :");
-	            mvwprintw(ins, 3, 2, "Move Left -> Left arrow key");
-	            mvwprintw(ins, 4, 2, "Move Right -> Right arrow key");
-	            mvwprintw(ins, 5, 2, "Quit game -> %c", control_quit);
+	        mvwprintw(ins, 3, 2, "Move Left -> Left arrow key");
+	        mvwprintw(ins, 4, 2, "Move Right -> Right arrow key");
+	        mvwprintw(ins, 5, 2, "Quit game -> %c", control_quit);
                 mvwprintw(ins, 6, 2, "blue normal tile: +1 hp");
                 mvwprintw(ins, 7, 2, "red spike tile: -3 hp");
                 mvwprintw(ins, 8, 2, "green spring tile: bouncy, +1 hp");
@@ -82,10 +96,12 @@ int menu()
                 mvwprintw(ins, 11,2, "magenta right conveyer tile: pushes right, +1 hp");
                 wrefresh(ins);
             }
+	    // Leaderboard choice : trigger the function that shows score board
             if(i == 2)
             {
                 //display the score
             }
+	    // exit choice : delete the menu windows and return to terminal
             if(i == 3)
             {
                 delwin(men);
@@ -94,6 +110,7 @@ int menu()
             }
             break;
         }
+	// highlights the next choice which the player is currently on
         wattron(men, A_STANDOUT);
         sprintf(choice, "%s", choice_list[i]);
         mvwprintw(men, i+1, 1, "%s", choice);
