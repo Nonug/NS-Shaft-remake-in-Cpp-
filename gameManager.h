f0c94c4435adb3476d9d8276afda287e29549583
e <65940458+Nonug@users.noreply.github.com>
2020-11-21 19:07:00 +0800

    Added comments and refactored code

--- gameManager.h
+++ gameManager.h
@@ -1,56 +1,50 @@
 #include "common.h"
 #include "tile.h"
 #include "player.h"
-#include "menu.h"
+#include "end.h"
 #ifndef _MANAGER_H_
 #define _MANAGER_H_
 
-// main: game.run()
+// Manager Object: Controls the flow of the game, entities and physics
 //
-// game: create player, create tile,
-//      constructor: call create funct
-//      run(): running = 1;
-//            while running
-//                start time count
-//                inputPhase, update, drawphase
-//                calculate elasped time, set fps
+//  main game control:  run, getInput
 //
+//  window control:     update_stat, update_inst, display
 //
-//  physics: collisionsCheck, movePlayer
-//  entity control: addTile, clearDeadTiles
-//  main game control: display, run, getInput
+//  physics:            collisionsCheck, movePlayer, effect, movePlayer
 //
+//  entity control:     addTile, clearDeadTiles, healthupdate
 //
-// event/entity/physics manager?
+
 class Manager
 {
   public:
     double tileSpawnY = MAX_Y - BHEIGHT + 5; // y of spawnpt of tile
-    int tileSpawnX = rand() % 60 + 1; //random xpos for tiles
-    double speed = 0.15; // speed of tiles
-    double spawnTileDelay; // delay between each tile spawn = 0.12 / speed
+    int tileSpawnX = rand() % 60 + 1;        // random xpos for tiles
+    double speed = 0.15;                     // speed of tiles
+    double spawnTileDelay;      // delay between each tile spawn = 0.12 / speed
 
-    bool running = 1; // while game run
+    bool running = 1;               // is game running?
 
+    WINDOW* playwin;                // game window, with tiles and player
+    WINDOW* statwin;                // show curent level, HP
+    WINDOW* instwin;                // show instruction
+    vector<unique_ptr<Tile>> tiles; // store generated alive tiles.
+    Player* p;                      // Player obj pointer
 
+    int levelCoeff = 100; // As game progress, it decreases.
+                          // this will increases tile speed and hostile tile spawn rate
 
-    WINDOW* playwin;
-    WINDOW* statwin; //!
-    WINDOW* instwin;
-    vector<unique_ptr<Tile>> tiles;
-    Player* p;
-    int diff = 1;
-    int levelCoeff = 100; // As game progress, it lowers.
-                          // which increases tile speed and hostile tile spawn rate
-
+    // class constructor, initialize windows and player
     Manager(){
       playwin = newwin(MAX_Y,MAX_X, 5 , 1); // playing window
-      statwin = newwin(9,24,5,MAX_X+4);     //statistics window!
-      instwin = newwin(14,55,15, MAX_X+4);
-       nodelay(playwin, true);
-      p = new Player(playwin,10,20);
+      statwin = newwin(9,24,5,MAX_X+4);     // statistics window (score, hp)
+      instwin = newwin(14,55,15, MAX_X+4);  // instructions window
+      nodelay(playwin, true);               // no delay for getch()
+      p = new Player(playwin,10,30);        // create a player obj
     }
 
+    // Class Destructor: release memory
     ~Manager(){
       delete p;
       delwin(playwin);
@@ -58,52 +52,118 @@ class Manager
       delwin(instwin);
     }
 
+    // Heart of the game, call other member functions
+    void run(){
+
+      using clock = chrono::system_clock;
+      using sec = chrono::duration<double>;
+      auto before = clock::now();           // set timestamp before each loop
+      spawnTileDelay = 120 / speed;         // delay between tile spawn
+      auto timelimit = spawnTileDelay * chrono::milliseconds{1};// convert to ms
+      srand(time(0));                       // set rand seed
+      update_inst();                        // draw the instruction once
+
+      // add a safety tile
+      tiles.emplace_back(new Tile(tileSpawnY, 30, speed));
+
+      while(running) // when not dead/ quit game
+      {
+        // Add a tile if time elapsed > spawnTileDelay
+        if (clock::now() - before > timelimit)
+        {
+          before = clock::now();
+          addTile();
+        }
+
+
+        clearDeadTiles(); // remove dead tiles
+        display();    // main physics function, perform actions then display it
+
+
+        // end the game if HP below 0
+        if (p->health <= 0)
+        {
+          running = 0;
+        }
+
+      }
+
+      // when dead or quit, wipe the screen and show end screen
+      clear();
+      refresh();
+      endscr(p->level); // from end.h
+
+    }
+
+    // get input, then call player member function to move it L/R
+    void getInput(){
+      int choice = getch();
+      switch(choice)
+      {
+        case 'a':
+          p->mvleft();
+          break;
+        case 'd':
+          p->mvright();
+          break;
+        case control_quit: // defined in common.h, default = 'q'
+          running = 0;
+          break;
+        default:
+          break;
+      }
+      return;
+    }
+
+    // call the physics functions, refresh the frame and entities
     void display(){
 
-      clear();
+      clear(); // clear previous frames (not shown yet)
 
+      // refresh all frames (show empty)
       refresh();
       wrefresh(playwin);
-      wrefresh(statwin); //!
+      wrefresh(statwin);
       wrefresh(instwin);
-      // wrefresh(playwin);
 
-      getInput(); //read user input
-      p->gravity();
-      effect();
-      movePlayer();
-      p->display();
 
+      getInput();     // read user input
+      p->gravity();   // gravity move player down
+      effect();       // interactions with tiles
+      healthupdate(); // update player HP
+      movePlayer();   // update player location
+      p->display();   // show player
+      update_stat(p->level, p->health); // update stat window data
+
+      // move and show every alive tiles, and show the player too
       for (const auto& tile : tiles){
         tile->move();
         tile->display();
         p->display();
       }
 
+      // game box with spikes from ceiling
       wborder(playwin, 0, 0, 'V', 0, 0, 0, 0, 0);
-      box(statwin,0,0);//!
+      // box for other windows
+      box(statwin,0,0);
       box(instwin,0,0);
 
-      // for (int i = 0 ; i < 4;i++)
-      // mvprintw(4,1+i*15,"|23456789abced|");
-
-
       refresh();
-      usleep(DELAY); // Shorter delay between movements
+      usleep(DELAY); // delay between movements, defined in common.h
     }
 
-    void update_stat(int level, int health, int diff) {
+    // update stat window with player lvl and hp
+    void update_stat(int level, int health) {
+
       box(statwin, 0, 0);
+      mvwprintw(statwin, 0, 6 , "# STATS #");
 	    mvwprintw(statwin, 2, 5, "HEALTH : %i", health);
-      // wmove(statwin, 3, 5);                // HP bar
-      // for (int i = 0; i < health; i++){
-      //   waddch(statwin, 'o');
-      // }
 	    mvwprintw(statwin, 4, 5, "LEVEL : %i", level);
-      mvwprintw(statwin, 6, 5, "DIFFICULTY : %i", diff);
-	    mvwprintw(statwin, 0, 6 , "# STATS #");
+      // mvwprintw(statwin, 6, 5, "DIFFICULTY : %i", diff);
+
     }
 
+    // Draw the instruction window
     void update_inst(){
       box(instwin,0,0);
       mvwprintw(instwin, 2, 2, "Controls :");
@@ -119,51 +179,15 @@ class Manager
     }
 
 
-    void run(){
-
-      using clock = chrono::system_clock;
-      using sec = chrono::duration<double>;
-      auto before = clock::now();
-      spawnTileDelay = 120 / speed;
-      auto timelimit = spawnTileDelay * chrono::milliseconds{1};
-      srand(time(0)); //set rand seed
-
-      tiles.emplace_back(new Tile(tileSpawnY, 17, speed));
-      while(running)
-      {
-
-        if (clock::now() - before > timelimit)
-        {
-          before = clock::now();
-          addTile();
-
-        }
-        clearDeadTiles();
-        display();
-        healthupdate();
-        update_stat(p->level, p->health, diff);
-        update_inst();
-        // checkseed(p->level);
-        if (p->health <= 0)
-        {
-          running = 0;
-        }
-
-      }
-      // delwin(playwin);//new
-      // endwin();       //new
-      //deconstructor here
-    }
-
 
     // generate tiles psuedo-randomly
     void addTile(){
       // chance of hostile tiles spawning increases as time goes on
 
-      // 59 e^{-\frac{x}{100}}\ +40
+      // f(x) = 59 e^{-\frac{x}{100}}\ +40
       levelCoeff = 59 * exp(-(p->level)/100) + 40; // from 99 to 40
-      speed = 0.002 * p->level + 0.15; // speed from 0.15 to 0.3
-      int seed = rand() % levelCoeff; // rand from 99 to 40, upperbound drops over time
+      speed = 0.002 * p->level + 0.15;        // speed from 0.15 to 0.3
+      int seed = rand() % levelCoeff;         // rand from 99 to 40, upperbound drops over time
       tileSpawnX = rand() % (MAX_X - BWIDTH); //random xpos for platforms
 
       if (seed > 50){         // 50% Normal
@@ -176,28 +200,31 @@ class Manager
         tiles.emplace_back(new LConveyerTile(tileSpawnY,tileSpawnX,speed));
       } else if (seed > 15){  // 15% Fragile
         tiles.emplace_back(new FragileTile(tileSpawnY,tileSpawnX,speed));
-      } else {                // 0 - 14 : Spike (15%)
+      } else {                // 15% Spike
         tiles.emplace_back(new SpikeTile(tileSpawnY,tileSpawnX,speed));
       }
 
     }
 
+    // remove dead tiles
     void clearDeadTiles(){
-      // remove dead tiles
-      // with an iterator using Erase-Remove Idiom
+      // delete all dead tiles an iterator using Erase-Remove Idiom
       tiles.erase(remove_if(tiles.begin(), tiles.end(),
                   [](auto& tile) // C++14: auto in lambda
                   {return tile->isDead;}),
                   tiles.end());
     }
 
-
+    // Check player and tile collisions
+    // return the first tile in collision with player, else return NULL
     Tile* collisionsCheck(){
-      p->invincibleCount -= 1;
+
       if (p->invincibleCount > 0){// player invincible, no collision allowed
-        p->inAir = 1; // no collisions at all
-        return NULL;
+        p->invincibleCount -= 1;
+        p->inAir = 1;
+        return NULL; // no collisions at all
       }
+
 
       for (auto& tile: tiles){
         double tileMaxY = tile->y + tile->height;
@@ -209,7 +236,6 @@ class Manager
             continue; // no collision
         else {
           p->inAir = 0; // has collision
-          // tile->isTouched = 1; relocated to hpupdate
           return tile.get(); // return RAW pointer to tile
         }
       }
@@ -220,19 +246,19 @@ class Manager
 
 
 
-    // effects of the tiles
+    // mechanics of the tiles
     void effect(){
       Tile* t = collisionsCheck();
+      // constansts defined from common.h
       if (t != NULL ){
-
-        // spring tile bounces upwards by 3 after some delay
+        // spring tile bounces player upwards by 3 after some delay
         if (t -> type == SPRING && t->touchCount >= 10){
           p->velocity[Y] -= 3;
           t->touchCount = 0; // reset delay
         }
-        // fragile tile are trap blocks that breaks after some delay
+        // fragile tile breaks after some delay
         if (t -> type == FRAGILE && t->touchCount >= 20){
-            t -> isDead = 1;
+            t -> isDead = 1; // dies after breaking
         }
         // left conveyer tile sets velocity to 0.2 units leftwards
         if (t -> type == LCONVEYER){
@@ -246,7 +272,7 @@ class Manager
     }
 
 
-
+    // update health of player
     void healthupdate()
     {
       Tile* t= collisionsCheck();
@@ -257,23 +283,23 @@ class Manager
       }
 
 
+      // end function if no collision
+      if (t == NULL) return;
 
-      if (t == NULL) return; // end function sicne no collision
-
-      // increment delay for spring and fragile tiles effect activation
+      // Increment delay for spring and fragile tiles effect activation
       if (t->type == SPRING || t->type == FRAGILE) t->touchCount += 2;
 
-      // if player standing on a tile and the tile is not being standed on previously
+      // if player standing on a tile which was not stood on previously
       if (t->isTouched != 1){
 
         t->isTouched = 1;       // mark as touched
         p->level += 1;          // add score
 
         if (t -> type == SPIKE){// spike tiles
-            p->health -= 3;
+            p->health -= 3;     // damage
         } else {                // other tiles
           if (p->health < MAX_HEALTH){
-            (p->health)++;         // heal
+            (p->health)++;      // heal
           }
         }
 
@@ -282,63 +308,24 @@ class Manager
     }
 
 
-
+    // set player new coordinates from velocity
     void movePlayer(){
       p->x += p->velocity[X];
-      Tile* t= collisionsCheck();
-      // if (t != NULL){
-      //     if (p->velocity[X] > 0){
-      //       // playerR = tileL
-      //       // playerX + playerW = tileX
-      //       //p->x = t->x - p->width;
-      //
-      //     }
-      //     if (p->velocity[X] < 0){
-      //       // playerL = tileR
-      //       // p->x = t->x + t->width;
-      //     }
-      // }
-
       p->y += p->velocity[Y];
       t = collisionsCheck();
       if (t != NULL){
-          // if fall down
+          // if falling down
         if (p->velocity[Y] > 0){
-          // playerDown = tileTop
-          // playerY + playerH = tileY
+          // set playerDown = tileTop
+          // i.e. playerY + playerH = tileY
+          // move player back to tile top
           p->y = t->y - p->height;
         }
 
-        // if (p->velocity[Y] < 0){
-        //   // playerDown = tileTop
-        //   // playerY + playerH = tileY
-        //   p->y = t->y + p->height;
-        // }
-
-      // p->velocity[X] = 0;
-      // p->velocity[Y] = 0;
       }
     }
 
-    int getInput(){
-      int choice = getch();
-      switch(choice)
-      {
-        case 'a':
-          p->mvleft();
-          break;
-        case 'd':
-          p->mvright();
-          break;
-        case control_quit:
-          running = 0; //TODO define and call destructors
-          break;
-        default:
-          // p->velocity[X] = 0;
-          break;
-      }
-      return choice;
-    }
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
-// square (y,x) = (2,3)
-// playwin: h,w = 22*2,24*3
-// player: 2*2, 2*3
-#include "common.h" // relocate incldues and global var
-#include "player.h"
-#include "tile.h"
-#include "menu.h"
-#include "gameManager.h"
-// #include <functional> // for unary_function
+#include "common.h" // contain library includes and global var
+#include "player.h" // player class
+#include "tile.h"   // tile class
+#include "menu.h"   // start menu
+#include "end.h"    // end menu
+#include "gameManager.h" // game controller class
 
-
+// Initialize ncurses standard screen, start colour
 void game_init(){
-  setlocale(LC_ALL, ""); // set locale to draw utf-8 characters
-  initscr(); // Initialize the window
+  setlocale(LC_ALL, ""); // set locale to draw unicode characters
+  initscr();             // Initialize the window
 
   // start colour if its supported
   if(has_colors() == FALSE)
@@ -20,25 +17,25 @@ void game_init(){
     printf("Your terminal does not support color\n");
     exit(1);
   }
-  start_color();			/* Start color 			*/
-  // init_pair(1, COLOR_RED, COLOR_BLACK);
-  // init_pair(tile_color, COLOR_YELLOW, COLOR_GREEN);
-  // init_pair(blank_color, COLOR_BLACK, COLOR_BLACK);
-  // init_pair(b_and_w, COLOR_WHITE, COLOR_BLACK);
 
-  raw();				/* Line buffering disabled	*/
+  start_color();		    	/* Start color 			*/
+
+  raw();			           	/* Line buffering disabled	*/
   keypad(stdscr, TRUE);		/* We get F1, F2 , arrow keys etc..		*/
-  noecho(); // Don't echo any keypresses
-  curs_set(FALSE); // Don't display a cursor
-  // //allow to animate screen while waiting input
-  nodelay(stdscr, true);
+  noecho();               // Don't echo any keypresses
+  curs_set(FALSE);        // Don't display a cursor
+  nodelay(stdscr, true);  //allow to animate screen while waiting input
 }
+
+// Main game loop.
 int main(int argc, char** argv) {
 
-    game_init(); // Initialize the screen
-    menu();
-    Manager* manager = new Manager(); // create the game manager and run
-    manager->run(); // main game phase, TODO: end screen)
-    delete manager;
-    endwin(); // end the process TODO: kill playwin, player etc...
+    game_init();                        // Initialize the screen
+    while (1){
+      menu();                           // start screen
+      Manager* manager = new Manager(); // create the game manager and run
+      manager->run();                   // main game phase
+      delete manager;                   // release memory
+    }
+    endwin();                           // end ncurses terminal
  }
